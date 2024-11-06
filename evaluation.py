709f9e6784508e7426f670983a0f270567b98f09
AndreasRaaskov <s183901@student.dtu.dk>
2024-11-06 17:37:29 +0100

    Updated evaluation

--- evaluation.py
+++ evaluation.py
@@ -16,6 +16,9 @@ import tqdm
 
 def main(cfg: DictConfig):
 
+    #Make the analysis object
+    logger = TrainingLogger(os.path.join(cfg.output_dir, 'Evaluation.json'))
+
     if cfg.device.lower() == "auto":
         device= "cuda" if torch.cuda.is_available() else "cpu"
         print(f"Using device: {device}")
@@ -25,21 +28,104 @@ def main(cfg: DictConfig):
     
     if cfg.mode == "Joint":
         #Load the model
+        model = torch.load(cfg.joint_path, map_location=torch.device(device))
+        model.eval()
+        model.use_sigmoid = True #line to stay compatible with the old models
+        model.to(device)
+
+
+    elif cfg.mode == "Independent" or cfg.mode == "Sequential":
+        # Load x to c model
+        XtoC_model = torch.load(cfg.XtoC_path, map_location=torch.device(device))
+        XtoC_model.eval()
+        XtoC_model.use_sigmoid = True
+        XtoC_model.to(device)
+
+        # Load c to y model
+        CtoY_model = torch.load(cfg.CtoY_path, map_location=torch.device(device))
+        CtoY_model.eval()
+    
+    elif cfg.mode == "Standard":
+
+        #Load the model
         model = torch.load(cfg.model_path, map_location=torch.device(device))
         model.eval()
-        model.set_sailency_output("C")
-
-        XtoC_test(model,device,cfg)
-
-
-        model.set_sailency_output("Y")
-
-        
-        
-
-
+        model.to(device)
     else:
         raise NotImplementedError(f"Model type {cfg.model_type} is not implemented")
+
+
+    #Make the dataset
+    transform = get_inception_transform(mode=cfg.split, methode= cfg.transform_method)
+    data_set = CUB_extnded_dataset(mode=cfg.split,config_dict=cfg.CUB_dataloader,transform=transform)
+
+    #Make a mask if a model need to be tested on another dataset than it was trained on
+    if cfg.CUB_mask.use:
+        mask = CUB_extnded_dataset(mode=cfg.split,config_dict=cfg.CUB_mask,transform=transform).concept_mask
+
+    # Calculate accuracy
+    for i in tqdm.tqdm(range(len(data_set))):
+
+        X, C, Y , _ = data_set[i]
+
+        if cfg.CUB_mask.use:
+            #Apply the mask
+            C = C[mask]
+
+        #Unsqueese the data and move it to the device
+        X = X.unsqueeze(0).to(device)
+        C = C.unsqueeze(0).to(device)
+        Y = Y.unsqueeze(0).to(device)
+
+        if cfg.mode == "Standard":
+            #Forward pass
+            Y_hat = model(X)
+        
+        else: # Evaluate both C and Y
+            if cfg.mode == "Joint":
+                #Forward pass
+                C_hat, Y_hat = model(X)
+            
+            elif cfg.mode == "Independent" or cfg.mode == "Sequential":
+                #Forward pass
+                C_hat = XtoC_model(X)
+                Y_hat = CtoY_model(C_hat)
+
+            if cfg.CUB_mask.use:
+                #Only evaluated masked concepts
+                C_hat = C_hat[..., mask]
+
+
+            #Update concept accuracy
+            logger.update_concept_accuracy(mode="test", predictions=C_hat, ground_truth=C)
+
+        #Update the class logger
+        logger.update_class_accuracy(mode="test",logits=Y_hat, correct_label=Y)
+
+    if cfg.sailency == True and cfg.mode != "Standard":
+
+        for i in tqdm.tqdm(range(len(data_set))):
+
+            X, C, Y , coordinates = data_set[i]
+
+            if cfg.CUB_mask.use:
+                #Apply the mask
+                C = C[mask]
+                coordinates = [coordinates[i] for i in mask]
+
+            concept_list,coordinates = get_visible_consepts(coordinates)
+
+            sailency_maps = get_saliency_maps(X,concept_list,model,method_type=cfg.sailency_methode)
+
+            sailency_score = saliency_score_image(sailency_maps,coordinates=coordinates)
+
+            logger.update_sailency_score(mode="test",score=sailency_score)
+
+    logger.log_metrics(i)
+        
+
+        
+
 
 
 def XtoC_test(model,device,cfg: DictConfig):
@@ -58,7 +144,7 @@ def XtoC_test(model,device,cfg: DictConfig):
     #Make a mask if a model need to be tested on another dataset than it was trained on
     if cfg.CUB_mask.use:
         mask = CUB_extnded_dataset(mode=cfg.split,config_dict=cfg.CUB_mask,transform=transform).concept_mask
-        device_mask = torch.tensor(mask).to(device)
+
     
 
 
