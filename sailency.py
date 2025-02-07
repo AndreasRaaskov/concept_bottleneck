464ec260852035e856f9bc43dba6cf2f9d40e8d0
AndreasRaaskov <s183901@student.dtu.dk>
2025-02-07 23:49:39 +0100

    Found another mistake in sailency score

--- sailency.py
+++ sailency.py
@@ -82,7 +82,7 @@ def saliency_score_part(saliency_map, coordinates):
     height, width = saliency_map.shape  # Get the dimensions of the saliency map
 
     # Create coordinate grids for the entire map
-    x_coords,y_coords = np.ogrid[:height, :width]
+    x_coords,y_coords = np.ogrid[height:0:-1, :width]
 
     distances = []
     
